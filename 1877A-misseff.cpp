f169401415b1721a24ed8d82548c1b3b0232a155
DevAryanSin <pineapplepenguin30@gmail.com>
2026-08-06 17:26:43 +0000

    cff

---
+++ 1877A-misseff.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+  int t;
+  cin >> t;
+
+  while(t--){
+    int n;
+    cin >> n;
+    long long arr[n];
+    long long sum =0;
+    for(int i =0;i < n-1;i++){
+      int x = 0;
+      cin >> x;
+      sum+= x;
+    }
+
+    cout << -1*sum << endl;
+
+
+  }
+
+  return 0;
+}
---
+++ 1878A-daytona.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+  int t;
+  cin >> t;
+  
+
+  while(t--){
+    int n,k;
+    cin >> n >> k;
+    int arr[n];
+    for(int i =0;i < n;i++){
+      cin >> arr[i];
+    }
+    bool present = false;
+    for(int i =0;i < n;i++){
+      if(arr[i] == k){ present = true; break;}
+    }
+    if(present) cout << "YES" << endl;
+    else cout << "NO" << endl;
+  }return 0;
+}
